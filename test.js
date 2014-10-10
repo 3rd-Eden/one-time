b111230b6f312d7b247f0d3dc8df3b888a6de1bb
Arnout Kazemier <info@3rd-Eden.com>
2014-10-10 08:59:46 +0200

    [test] Add test coverage

--- test.js
+++ test.js
@@ -3,16 +3,19 @@
 var one = require('./')
   , called = 0;
 
-var callme = one(function maybe(bar) {
-  called++;
+/* istanbul ignore next */
+(function () {
+  var callme = one(function maybe(bar) {
+    called++;
 
-  if (bar !== 'bar') throw new Error('Invalid argument received');
-  return 'foo';
-});
+    if (bar !== 'bar') throw new Error('Invalid argument received');
+    return 'foo';
+  });
 
-if (callme('bar') !== 'foo') throw new Error('Invalid returned state');
-if (callme('bar') !== 'foo') throw new Error('Invalid returned state');
-if (callme('bar') !== 'foo') throw new Error('Invalid returned state');
-if (callme('bar') !== 'foo') throw new Error('Invalid returned state');
+  if (callme('bar') !== 'foo') throw new Error('Invalid returned state');
+  if (callme('bar') !== 'foo') throw new Error('Invalid returned state');
+  if (callme('bar') !== 'foo') throw new Error('Invalid returned state');
+  if (callme('bar') !== 'foo') throw new Error('Invalid returned state');
 
-if (called !== 1) throw new Error('Called multiple times');
+  if (called !== 1) throw new Error('Called multiple times');
+}());
