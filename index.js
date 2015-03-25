646e3445b2f3bdc60888e15f5a1434efdbe5c8b0
Arnout Kazemier <info@3rd-Eden.com>
2015-03-25 14:06:51 +0100

    [fix] Set the same function name as the supplied function for debug purposes

--- index.js
+++ index.js
@@ -8,16 +8,30 @@
  * @api public
  */
 module.exports = function one(fn) {
-  var called = false
+  var called = 0
     , value;
 
-  return function time() {
+  /**
+   * The function that prevents double execution.
+   *
+   * @api private
+   */
+  function onetime() {
     if (called) return value;
 
-    called = true;
+    called = 1;
     value = fn.apply(this, arguments);
     fn = null;
 
     return value;
-  };
+  }
+
+  //
+  // To make debugging more easy we want to use the name of the supplied
+  // function. So when you look at the functions that are assigned to event
+  // listeners you don't see a load of `onetime` functions but actually the
+  // names of the functions that this module will call.
+  //
+  onetime.displayName = fn.displayName || fn.name || onetime.displayName || onetime.name;
+  return onetime;
 };
